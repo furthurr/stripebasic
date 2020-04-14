a3ad22a68d51d981f5722e90f0ac668b5ed8174c
furthurr <myfurthur@gmail.com>
2020-04-14 15:16:10 -0500

    create main
    simple main

---
+++ main.dart
@@ -0,0 +1,70 @@
+import 'package:appname/stripe_provider.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _stripe                 = new Stripe();
+  String _token               = 'Sin token';
+  String _numberCard          = '4000056655665556';
+  String _cardMonthExpiration = '12';
+  String _cardYearExpiration  = '2024';
+  String _cardCvc             = '123';
+  String _idCharge            = 'Primero necesitas generar un token';
+  String _secretKey           = 'sk_test_Af08tUBa....';
+  String _monto               = '5000000';
+  String _concepto            = 'invitale un cafe a @furthurr';
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    return MaterialApp(
+      title: 'Simple Stripe @furthurr',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple Stripe @furthurr'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('card: $_numberCard'),
+              Text('month_expiration: $_cardMonthExpiration'),
+              Text('year_expiration: $_cardYearExpiration'),
+              Text('cvc: $_cardCvc'),
+              RaisedButton(onPressed: getToken,
+              child: Text('generar token'),),
+              Text('token: $_token'),
+              RaisedButton(onPressed: setCharge,
+              child: Text('Generar pago'),),
+              Text('Id de cobro: $_idCharge')
+            ],
+          ),
+        ),
+      ),
+    ); 
+  }
+  void getToken() {
+      _stripe.cardToken(_numberCard,_cardYearExpiration,_cardMonthExpiration,_cardCvc,_secretKey).then((data){
+        setState(() {
+          _token = data;            
+        });
+      });
+  }
+  void setCharge(){
+    if (_token=='Sin token'){print('no puedes realizar pago');}
+    else{
+      _stripe.createCharge(_token,_monto,_concepto,_secretKey).then((data){
+        setState(() {          
+          _idCharge = data;
+        });
+      });
+    }
+  }
+}
